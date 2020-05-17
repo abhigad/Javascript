b7c5a8d35607f97c13d1529f1b45276dfcae3336
abhijit <abhijit.gadkari@gmail.com>
2020-05-16 23:10:41 -0700

    browserify support included

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-//var moment = require("moment");
+var moment = require("moment");
+var unique = require('uniq');
 var myDate = new Date();
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+console.log(unique(data));
 console.log(myDate);
-//var mycoolDate = moment(myDate).format("LL");
-//console.log(mycoolDate);
+var mycoolDate = moment(myDate).format("LL");
+console.log(mycoolDate);
